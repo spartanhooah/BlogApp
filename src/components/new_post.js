60c7dac3e80685e97d0fd9f231a43e3efa99edb1
Ben Frey <spartanhooah@gmail.com>
2017-05-20 19:55:04 -0500

    Form done except for cancel button and actual submission.

--- src/components/new_post.js
+++ src/components/new_post.js
@@ -1,13 +1,75 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class NewPost extends Component {
-    render() {
-        return (
-            <div>
-                New Post!
+    renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${touched  && error ? 'has-danger' : ''}`;
+
+        return(
+            <div className={className}>
+                <label>{field.label}</label>
+                <input className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
+        );
+    }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
+    render() {
+        const {handleSubmit} = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         );
     }
 }
 
-export default NewPost;
+function validate(values) {
+    const errors = {};
+
+    // Validate inputs from values object
+    if(!values.title) {
+        errors.title = "Enter a title";
+    }
+
+    if(!values.categories) {
+        errors.categories = "Enter at least one category";
+    }
+
+    if(!values.content) {
+        errors.content = "Enter some content";
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'NewPostForm'
+})(NewPost);
